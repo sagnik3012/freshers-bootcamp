48e7aee506a8d70abe9c9687b082d8e5b8062780
sagnik saha <sagnik3012@gmail.com>
2021-07-21 19:37:56 +0530

    improved  formatting

--- day1/exercise1/solution.go
+++ day1/exercise1/solution.go
@@ -6,53 +6,59 @@ import (
 )
 
 type Matrix struct{
-	numRows int
-	numCols int
-	data [][] int
+	NumRows int `json : "rows"`
+	NumCols int `json : "cols"`
+	Data [][] int `json :"data"`
 }
 
 func (m Matrix) getNumCols() int{
-	return m.numCols
+	return m.NumCols
 }
 
 func (m Matrix) getNumRows() int{
-	return m.numRows
+	return m.NumRows
 }
 
 func (m *Matrix) set( row , col , value int) {
-	m.data[row][col] = value
+	m.Data[row][col] = value
 	return
 }
+
 func (m1 *Matrix) addMatrices(m2 Matrix) Matrix{
 
-	rows := m1.numRows
-	cols := m1.numCols
+	rows := m1.NumRows
+	cols := m1.NumCols
 	for row := 0 ; row < rows ; row++{
 		for col := 0 ; col < cols ; col ++ {
-			m1.data[row][col] = m1.data[row][col] + m2.data[row][col]
+			m1.Data[row][col] = m1.Data[row][col] + m2.Data[row][col]
 		}
 	}
 	return *m1
 }
-func matrixToJson(m Matrix){
-	matrix , err := json.MarshalIndent(m.data,""," ")
+
+func matrixToJson(m Matrix) error {
+	matrix , err := json.MarshalIndent(m,""," ")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	fmt.Println(string(matrix))
+	return nil
 }
-
-
-
 
 func main(){
 
 	mat1 := Matrix{2,3,[][] int {{1,2,3},{4,5,6}},}
 	mat2 := Matrix{2,3,[][] int {{4,5,6},{1,2,3}},}
+
 	mat1.addMatrices(mat2)
 	fmt.Println("no. of rows in mat1 = ",mat1.getNumRows())
 	fmt.Println("no. of cols in mat2 = ",mat2.getNumCols())
+
 	mat1.set(1,1,100)
-	matrixToJson(mat1)
+
+	err := matrixToJson(mat1)
+	if err != nil{
+		fmt.Println("Error in converting matrix to json ",err)
+	}
 }
 
