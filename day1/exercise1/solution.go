ea5c2188ab45d640f5fcfb860595ab92b32b4b18
sagnik saha <sagnik3012@gmail.com>
2021-07-21 12:58:08 +0530

    solution to matrix problem

--- day1/exercise1/solution.go
+++ day1/exercise1/solution.go
@@ -1,58 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
 type Matrix struct{
-	num_rows int
-	num_cols int
-	array [10][10] int
+	numRows int
+	numCols int
+	data [][] int
 }
 
-func (m Matrix) get_num_cols() int{
-	return m.num_cols
+func (m Matrix) getNumCols() int{
+	return m.numCols
 }
 
-func (m Matrix) get_num_rows() int{
-	return m.num_rows
+func (m Matrix) getNumRows() int{
+	return m.numRows
 }
 
 func (m *Matrix) set( row , col , value int) {
-	m.array[row][col] = value
+	m.data[row][col] = value
 	return
 }
-func (m1 Matrix) addMatrices(m2 Matrix) Matrix{
-	var sum = Matrix{}
-	rows := m1.num_rows
-	cols := m1.num_cols
+func (m1 *Matrix) addMatrices(m2 Matrix) Matrix{
+
+	rows := m1.numRows
+	cols := m1.numCols
 	for row := 0 ; row < rows ; row++{
 		for col := 0 ; col < cols ; col ++ {
-			sum.array[row][col] = m1.array[row][col] + m2.array[row][col]
+			m1.data[row][col] = m1.data[row][col] + m2.data[row][col]
 		}
 	}
-
-	return sum
+	return *m1
+}
+func matrixToJson(m Matrix){
+	matrix , err := json.MarshalIndent(m.data,""," ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(matrix))
 }
 
-func main(){
-	//var x Matrix [2][3] int
-	//c := x.get_num_cols()
-	var rows,cols int
-	var mat1 = Matrix{}
-	var mat2 = Matrix{}
-	var mat3 = Matrix{}
-	fmt.Println("Enter number of rows :")
-	fmt.Scanf("%d",&rows)
-	fmt.Println("Enter number of columns : ")
-	fmt.Scanf("%d",&cols)
-	mat1.num_rows = rows
-	mat1.num_cols = cols
 
-	mat2.num_rows = rows
-	mat2.num_cols = cols
-	mat1.set(2,3,999)
-	mat3 = mat1.addMatrices(mat2)
-	fmt.Println(mat3.array)
+
+
+func main(){
+
+	mat1 := Matrix{2,3,[][] int {{1,2,3},{4,5,6}},}
+	mat2 := Matrix{2,3,[][] int {{4,5,6},{1,2,3}},}
+	mat1.addMatrices(mat2)
+	fmt.Println("no. of rows in mat1 = ",mat1.getNumRows())
+	fmt.Println("no. of cols in mat2 = ",mat2.getNumCols())
+	mat1.set(1,1,100)
+	matrixToJson(mat1)
 }
 
