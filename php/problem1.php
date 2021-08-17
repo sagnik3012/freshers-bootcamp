f208490ec78165a0a4dbf47c0b1de0c43e43ab4c
sagnik saha <sagnik3012@gmail.com>
2021-08-17 16:10:17 +0530

    added solution to problem1

--- php/problem1.php
+++ php/problem1.php
@@ -1,1 +1,23 @@
 <?php
+
+
+$input = [2, 3, [4, 5], [6, 7], 8];
+$output = [];
+
+foreach ($input as $key => $value) {
+
+    if (is_array($value)) {
+        $output = array_merge($output, $value);
+    } else {
+        array_push($output, $value);
+    }
+
+}
+
+
+echo "Output \n";
+print_r($output);
+
+
+
+
